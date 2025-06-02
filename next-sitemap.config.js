46f13c1ece02f3c571c84c7c4a6627b5d6ea8799
slayer0x10 <saicharanruka@gmail.com>
2025-06-02 18:13:29 +0530

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: "https://saicharanruka.me", // 🔁 Replace with your actual domain
+	generateRobotsTxt: true, // ✅ Also generate robots.txt
+	sitemapSize: 7000,
+	changefreq: "weekly",
+	priority: 0.7,
+};

0a2537106ef800fb0f1dbe52570d270a3d7de59b
slayer0x10 <saicharanruka@gmail.com>
2025-06-02 18:02:01 +0530

    fixed card border

--- src/components/card/HowItWorkCard.tsx
+++ src/components/card/HowItWorkCard.tsx
@@ -44,15 +44,15 @@ export function HowItWorksCard() {
 
 	return (
 		<Card className="h-[220px] col-span-1 md:col-span-4 " heading="How I Work">
-			<div className="flex flex-col gap-1 p-2">
+			<div className="flex flex-col gap-1 p-2 ">
 				<h4 className="text-xl font-semibold">{steps[currentStep].title}</h4>
 				<p className="text-xs font-medium text-black/50 h-16">
 					{" "}
 					{steps[currentStep].content}{" "}
 				</p>
 
-				<Pagination>
-					<PaginationContent className="flex justify-center bg-white border rounded-full">
+				<Pagination className="-mt-3 md:mt-0">
+					<PaginationContent className="flex  justify-center bg-white border rounded-full">
 						{steps.map((step, index) => (
 							<PaginationItem key={index}>
 								<PaginationLink
